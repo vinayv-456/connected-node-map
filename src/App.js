425853d0cc8e87379ce0a10b6ce5a49f3eaf6025
vinayv-456 <vinayvallabineni456@gmail.com>
2022-02-20 11:00:04 +0530

    intial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import ForceGraph2D from "react-force-graph-2d";
+import { sampleNodalMap } from "./react_nodalmap_test_data";
 
 function App() {
+  const [graphData, setGraphData] = useState({});
+  useEffect(() => {
+    console.log("sampleNodalMap", sampleNodalMap);
+    let nodes = [];
+    let links = [];
+    sampleNodalMap.forEach((org) => {
+      nodes = [
+        ...nodes,
+        { ...org.organization, id: org.organization.cin, val: 5 },
+        ...org.members.map((member) => ({ ...member, id: member.din, val: 1 })),
+      ];
+      const organizationId = org.organization.cin;
+      links = [
+        ...links,
+        ...org.members.map((member) => ({
+          source: organizationId,
+          target: member.din,
+        })),
+      ];
+    });
+    console.log("nodes", nodes, "links", links);
+    setGraphData({
+      nodes,
+      links,
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ForceGraph2D graphData={graphData} />
     </div>
   );
 }
